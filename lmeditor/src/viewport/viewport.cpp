f8ec41c9fe3d498ada1d6f8c278178918c3aa5fe
lawrencem <lawrencem99@gmail.com>
2020-03-13 21:00:28 +1100

    Remove unused viewport methods.

--- lmeditor/src/viewport/viewport.cpp
+++ lmeditor/src/viewport/viewport.cpp
@@ -1,77 +1,7 @@
 #include "../viewport/viewport.h"
-#include <lmlib/variant_visitor.h>
 
 namespace lmeditor
 {
-
-bool viewport::control_camera(const lmtk::mouse_move_event &msg)
-{
-    if (msg.input_state.mouse_state[lmpl::mouse_button::M])
-    {
-        auto delta_mouse_pos = msg.delta;
-
-        if (msg.delta.width || msg.delta.height)
-        {
-            camera.rotate({delta_mouse_pos.height * 0.002f,
-                           delta_mouse_pos.width * 0.002f});
-            return true;
-        }
-    }
-    return false;
-}
-
-bool viewport::control_camera(const lmtk::mouse_button_down_event &msg)
-{
-    switch (msg.button)
-    {
-    case lmpl::mouse_button::U:
-        move_camera_closer(0.1f);
-        return true;
-
-    case lmpl::mouse_button::D:
-        move_camera_further(0.1f);
-        return true;
-
-    default:
-        return false;
-    }
-}
-
-bool viewport::control_camera(const lmtk::key_down_event &msg)
-{
-    if (msg.input_state.key_state.have_modifiers())
-        return false;
-
-    switch (msg.key)
-    {
-    case lmpl::key_code::F:
-        camera.rotate({0.f, -0.05f});
-        return true;
-
-    case lmpl::key_code::D:
-        camera.rotate({-0.05f, 0.f});
-        return true;
-
-    case lmpl::key_code::S:
-        camera.rotate({0.f, 0.05f});
-        return true;
-
-    case lmpl::key_code::E:
-        camera.rotate({0.05f, 0.f});
-        return true;
-
-    case lmpl::key_code::R:
-        move_camera_closer(0.1f);
-        return true;
-
-    case lmpl::key_code::W:
-        move_camera_further(0.1f);
-        return true;
-
-    default:
-        return false;
-    }
-}
 
 void viewport::move_camera_closer(const float amount)
 {
@@ -85,11 +15,4 @@ void viewport::move_camera_further(const float amount)
 
 viewport::viewport(viewport_init const &init) : camera{init.camera_init} {}
 
-bool viewport::handle(const lmtk::input_event &event)
-{
-    bool dirty = event >> lm::variant_visitor{
-                            [this](auto msg) { return control_camera(msg); },
-                          };
-    return dirty;
-}
 } // namespace lmeditor
--- lmeditor/src/viewport/viewport.h
+++ lmeditor/src/viewport/viewport.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <lmeditor/orbital_camera.h>
-#include <lmtk/lmtk.h>
 
 namespace lmeditor
 {
@@ -10,18 +9,9 @@ class viewport
 {
   public:
     viewport(viewport_init const &init);
-    bool handle(const lmtk::input_event &event);
 
     void move_camera_closer(const float amount);
     void move_camera_further(const float amount);
-
-    bool control_camera(const lmtk::key_down_event &msg);
-    bool control_camera(const lmtk::mouse_move_event &msg);
-    bool control_camera(const lmtk::mouse_button_down_event &msg);
-    template <typename message_type> bool control_camera(message_type msg)
-    {
-        return false;
-    }
 
     orbital_camera camera;
 };

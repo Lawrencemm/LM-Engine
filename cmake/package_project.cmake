a69e225b8b87fcbde3a74316f1b354d70e51761c
Lawrence Millar-Madigan <lawrencem99@gmail.com>
2021-12-26 11:47:52 +1100

    Make sample standalone in-source build
    This allows using the same assets relative path in both development and shipping builds.

--- cmake/package_project.cmake
+++ cmake/package_project.cmake
@@ -1,18 +1,17 @@
-macro(LM_PACKAGE TARGET)
-  set(INSTALL_BASE ${CMAKE_CURRENT_BINARY_DIR}/packaged)
+macro(LM_PACKAGE TARGET INSTALL_BASE)
   install(TARGETS ${TARGET} RUNTIME DESTINATION ${INSTALL_BASE}/bin)
   install(DIRECTORY assets/ DESTINATION ${INSTALL_BASE}/assets)
   set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION ${INSTALL_BASE}/bin)
   include(InstallRequiredSystemLibraries)
   install(CODE
       "
-      file(GLOB DLLS ${CMAKE_BINARY_DIR}/*.dll)
+      file(GLOB DLLS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/*.dll)
       file(COPY \${DLLS}\ DESTINATION ${INSTALL_BASE}/bin)
     "
   )
   install(CODE
       "
-    file(GLOB PDBS ${CMAKE_BINARY_DIR}/*.pdb)
+    file(GLOB PDBS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/*.pdb)
     file(COPY \${PDBS}\ DESTINATION ${INSTALL_BASE}/bin)
       "
   )
